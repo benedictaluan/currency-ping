ba6ea1c57fc2b359045faed0579b99e2d031279a
Benedict Aluan <benedict.aluan@gmail.com>
2015-11-06 00:37:54 +1300

    Updated sender domain.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: 'hello@findings.co'
+  default from: 'hello@mail.findings.co'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
